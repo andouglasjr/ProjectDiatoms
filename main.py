07541d6d702830f0648a89c4ac6426e12fd6a922
andouglas <andouglasjr@gmail.com>
2018-10-19 15:09:54 +0200

    Right results

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ data_transforms = {
         transforms.RandomRotation(180),
         #transforms.Grayscale(1),
         transforms.ToTensor(),
-        #transforms.Normalize([0.5017087, 0.5017087, 0.5017087], [0.08929635, 0.08929635, 0.08929635])
+        transforms.Normalize([0.5017087, 0.5017087, 0.5017087], [0.08929635, 0.08929635, 0.08929635])
         #transforms.Normalize([0.493], [0.085])
     ]),
     list_of_name_folders[1]: transforms.Compose([
@@ -22,17 +22,9 @@ data_transforms = {
         transforms.CenterCrop(224),
         #transforms.Grayscale(1),
         transforms.ToTensor(),
-        #transforms.Normalize([0.5017087, 0.5017087, 0.5017087], [0.08929635, 0.08929635, 0.08929635])
+        transforms.Normalize([0.5017087, 0.5017087, 0.5017087], [0.08929635, 0.08929635, 0.08929635])
         #transforms.Normalize([0.496], [0.07])
-    ]),
-    'test': transforms.Compose([
-        transforms.Resize(256),
-        transforms.CenterCrop(224),
-        #transforms.Grayscale(1),
-        transforms.ToTensor()
-        #transforms.Normalize([0.496, 0.496, 0.496], [0.07, 0.07, 0.07])
-        #transforms.Normalize([0.496], [0.07])
-    ]),
+    ])
 }
 
 #------------------------------------------------
@@ -43,23 +35,28 @@ data_dir = '../data'
 
 model_name='Resnet50'
 
-data = DataUtils(list_of_name_folders, data_dir, data_transforms, net_name = model_name)
-image_datasets = data.get_all_image_datasets()
-dataloaders = data.load_data(image_datasets)
-dataset_size = data.get_dataset_size()
+test_names = ['Resnet18','Resnet50']
 
-#Prameters of training
-params = {
-    'lr' : 0.001,
-    'momentum' : 0.9,
-    'step_size' : 4,
-    'gamma' : 0.00001,
-    'set_criterion' : True,
-    'num_epochs' : 8
-}
+for t in test_names:
 
-model_ft = ModelClass(model_name=model_name, folder_names = list_of_name_folders)
-model = model_ft.get_model()
-best_model = model_ft.train_model(model, dataloaders, params, dataset_size, data)
-model_ft.save_model(best_model, 'results/Resnet50_no_normalization.pt')
+    data = DataUtils(list_of_name_folders, data_dir, data_transforms, net_name = t)
+    image_datasets = data.get_all_image_datasets()
+    dataloaders = data.load_data(image_datasets)
+    dataset_size = data.get_dataset_size()
+
+    #Prameters of training
+    params = {
+        'lr' : 0.001,
+        'momentum' : 0.9,
+        'step_size' : 4,
+        'gamma' : 0.00001,
+        'set_criterion' : True,
+        'num_epochs' : 8
+    }
+
+
+    model_ft = ModelClass(model_name=t, folder_names = list_of_name_folders)
+    model = model_ft.get_model()
+    best_model = model_ft.train_model(model, dataloaders, params, dataset_size, data)
+    model_ft.save_model(best_model, 'results/' + t + '.pt')
 
