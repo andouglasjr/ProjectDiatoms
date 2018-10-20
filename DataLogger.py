f0ffcf51f0d157ec5b27bfb673434dc6bca6d6a5
andouglasjr <andouglasjr@gmail.com>
2018-10-20 16:03:33 +0200

    DataLogger

---
+++ DataLogger.py
@@ -0,0 +1,17 @@
+class DataLogger:
+    
+    def __init__(self):
+        self.file_log = open('results/log.dat','w')
+    
+    def log(self, content, log, close = False):
+        if(log == 'l'):
+            print("[!] {}".format(content))
+            self.file_log.write("[!] {}\n".format(content))
+        elif(log == 'v'):
+            print("[v] {}".format(content))
+            self.file_log.write("[!] {}\n".format(content))
+        elif(log == 'e'):
+            print("----> {}".format(content))
+            self.file_log.write("----> {}\n".format(content))
+        if close:
+            self.file_log.close()
